ad71a89207dadeb51301208934627612915c9037
Irvel <irvelnduva@hotmail.com>
2015-03-19 01:54:18 -0600

    Fixed the get word function

--- script.js
+++ script.js
@@ -6,10 +6,25 @@
   Marzo 2015
 */
 
-function cambiarP(){
+function cambiardP(){
   var xmlHttp = null;
   xmlHttp = new XMLHttpRequest();
-  xmlHttp.open( "GET", "http://randomword.setgetgo.com/get.php", false );
-  xmlHttp.send( null );
-  var casilla = document.getElementById("palabraInventada").innerHTML = xmlHttp.responseText;
+  xmlHttp.open( "GET", "http://randomword.setgetgo.com/get.php", true );
+  xmlHttp.send();
+  document.getElementById("palabraInventada").innerHTML = xmlHttp.responseText;
 };
+
+ function cambiarP() {
+        var requestStr = "http://randomword.setgetgo.com/get.php";
+
+        $.ajax({
+            type: "GET",
+            url: requestStr,
+            dataType: "jsonp",
+            jsonpCallback: 'RandomWordComplete'
+        });
+    };
+
+    function RandomWordComplete(data) {
+        document.getElementById("palabraInventada").innerHTML = data.Word;
+    };
